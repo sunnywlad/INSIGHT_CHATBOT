6eab34fe18958b20aa56ad1ef1c283bdd315c357
Wladimir Blaise <wld.world@tuta.io>
2026-03-17 19:27:27 +0100

    generate product model

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
-  resources :chats, only: [:index, :new, :create, :show]
+  resources :chats, only: [:index, :new, :create, :show] do
+    resources :messages, only: [:new, :create]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_13_184534) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_17_181011) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,13 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_13_184534) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["chat_id"], name: "index_messages_on_chat_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "brand"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
