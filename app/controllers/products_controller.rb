da42f633448ee8e9c3a60d85ee314f81f795506d
Wladimir Blaise <wld.world@tuta.io>
2026-03-23 18:12:23 +0100

    possibility of deleting products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.user = current_user
     @product.name = @product.name.titleize.strip
     @product.brand = @product.brand.upcase.strip
     if @product.save
@@ -21,6 +22,18 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @chats = @product.chats.where(user: current_user)
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    referer = request.referer
+    @product.destroy
+
+    if referer&.include?(product_path(@product))
+      redirect_to products_path, notice: "Product deleted."
+    else
+      redirect_back_or_to products_path, notice: "Product deleted."
+    end
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_17_230451) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_23_164621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_17_230451) do
     t.string "brand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,4 +68,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_17_230451) do
   add_foreign_key "chats", "users"
   add_foreign_key "insights", "chats"
   add_foreign_key "messages", "chats"
+  add_foreign_key "products", "users"
 end
